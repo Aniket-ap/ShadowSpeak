a64a5fac1259c45649228a3032da1318c6791625
Aniket Pradhan <aniketpradhan814@gmail.com>
2024-07-23 23:05:21 +0530

    changed minor

--- src/model/User.ts
+++ src/model/User.ts
@@ -39,10 +39,6 @@ const UserSchema: Schema<User> = new Schema({
     type: String,
     required: [true, "Email is required"],
     unique: true,
-    match: [
-      /.^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "please use a valid email address",
-    ],
   },
   password: {
     type: String,
